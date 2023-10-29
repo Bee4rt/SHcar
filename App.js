2c8f918cb218cc738b0983149e63eb1e8d7e7d9a
Bee4rt <info.visuel@gmail.com>
2023-10-29 23:00:06 +0200

    create first page

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+const img = require("./assets/images/bg.jpg")
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={img}
+          style={styles.image}
+        />
+
+        <View style={styles.titles}>
+        <Text style={styles.title}>Mercedes</Text>
+        <Text style={styles.subtitle}>Starting at $60,450</Text>
+        </View>
+
+      </View>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +33,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+    
+  titles: {
+    marginTop: '45%',
+    width: '100%',
+    alignItems: 'center',
+
+  },
+    
+  title: {
+    fontSize: 40,
+    fontWeight: '600',
+    color: 'white',
+    
+
+  },
+    
+  subtitle: {
+    fontSize: 18,
+    color: '#ccf'
+   
+
+  },
+  image:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  }
+
 });
